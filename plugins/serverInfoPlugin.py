9a80653c341ad6d3605c2e9e3020b0b1d417e7b4
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2024-04-26 22:21:09 +0200

    And yet another test plugin

---
+++ plugins/serverInfoPlugin.py
@@ -0,0 +1,40 @@
+import discord, requests
+from discord.ext import commands
+from discord import app_commands
+from configparser import ConfigParser
+
+has = ConfigParser()
+
+class serverInfoPlugin(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    
+    @commands.Cog.listener("on_message")
+    async def serverinfoplugin_commands(self, message):
+        has.read('server_config.ini')
+        if "serverInfoPlugin" in has.get(str(message.author.guild.id), 'plugins'):
+            if message.content == "!serverinfo":
+                server = message.author.guild
+                roles_count = len(server.roles)
+                channels_count = len(server.channels)
+                members_count = server.member_count
+                created_at = server.created_at.strftime("%B %d, %Y")
+                
+                embed = discord.Embed(title="Server Information", color=discord.Color.blurple())
+                embed.set_thumbnail(url=server.icon_url)
+                embed.add_field(name="Server Name", value=server.name, inline=False)
+                embed.add_field(name="Server ID", value=server.id, inline=False)
+                embed.add_field(name="Owner", value=server.owner, inline=False)
+                embed.add_field(name="Region", value=str(server.region).title(), inline=False)
+                embed.add_field(name="Members Count", value=members_count, inline=False)
+                embed.add_field(name="Roles Count", value=roles_count, inline=False)
+                embed.add_field(name="Channels Count", value=channels_count, inline=False)
+                embed.add_field(name="Creation Date", value=created_at, inline=False)
+
+                await message.send(embed=embed)
+                
+            
+async def setup(bot):
+    await bot.add_cog(serverInfoPlugin(bot))
+
+
